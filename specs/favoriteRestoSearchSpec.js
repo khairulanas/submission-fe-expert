8c560f551915474a5a4def0b41ab8547fc833627
khairul anas <mafiakanan@gmail.com>
2021-01-31 07:56:50 +0700

    Aku Suka Apa Yang Aku Lihat

--- specs/favoriteRestoSearchSpec.js
+++ specs/favoriteRestoSearchSpec.js
@@ -3,6 +3,7 @@ import FavoriteRestoIdb from '../src/scripts/data/favorite-resto-idb';
 
 describe('Searching restos', () => {
   let presenter;
+  let favoriteRestos;
 
   const searchRestos = (query) => {
     const queryElement = document.getElementById('query');
@@ -22,9 +23,9 @@ describe('Searching restos', () => {
         `;
   };
   const constructPresenter = () => {
-    spyOn(FavoriteRestoIdb, 'searchRestos');
+    favoriteRestos = spyOnAllFunctions(FavoriteRestoIdb);
     presenter = new FavoriteRestoSearchPresenter({
-      favoriteRestos: FavoriteRestoIdb,
+      favoriteRestos,
     });
   };
 
@@ -33,74 +34,98 @@ describe('Searching restos', () => {
     constructPresenter();
   });
 
-  it('should be able to capture the query typed by the user', () => {
-    searchRestos('resto a');
+  describe('When query is not empty', () => {
+    it('should be able to capture the query typed by the user', () => {
+      searchRestos('resto a');
 
-    expect(presenter.latestQuery).toEqual('resto a');
-  });
+      expect(presenter.latestQuery).toEqual('resto a');
+    });
 
-  // test double; test double
-  it('should ask the model to search for liked restos', () => {
-    searchRestos('resto a');
+    // test double; test double
+    it('should ask the model to search for liked restos', () => {
+      searchRestos('resto a');
 
-    expect(FavoriteRestoIdb.searchRestos)
-      .toHaveBeenCalledWith('resto a');
-  });
+      expect(favoriteRestos.searchRestos)
+        .toHaveBeenCalledWith('resto a');
+    });
 
-  it('should show the found restos', () => {
-    presenter._showFoundRestos([{ id: 1, title: 'Satu' }]);
-    expect(document.querySelectorAll('.resto__title').item(0).textContent)
-      .toEqual('Satu');
+    it('should show the found restos', () => {
+      presenter._showFoundRestos([{ id: 1, title: 'Satu' }]);
+      expect(document.querySelectorAll('.resto__title').item(0).textContent)
+        .toEqual('Satu');
 
-    presenter._showFoundRestos(
-      [{ id: 1, title: 'Satu' }, { id: 2, title: 'Dua' }],
-    );
+      presenter._showFoundRestos(
+        [{ id: 1, title: 'Satu' }, { id: 2, title: 'Dua' }],
+      );
 
-    const restoTitles = document.querySelectorAll('.resto__title');
-    expect(restoTitles.item(0).textContent).toEqual('Satu');
-    expect(restoTitles.item(1).textContent).toEqual('Dua');
-  });
+      const restoTitles = document.querySelectorAll('.resto__title');
+      expect(restoTitles.item(0).textContent).toEqual('Satu');
+      expect(restoTitles.item(1).textContent).toEqual('Dua');
+    });
 
-  it('should show - for found resto without title', () => {
-    presenter._showFoundRestos([{ id: 1 }]);
+    it('should show - for found resto without title', () => {
+      presenter._showFoundRestos([{ id: 1 }]);
 
-    expect(document.querySelectorAll('.resto__title').item(0).textContent)
-      .toEqual('-');
-  });
+      expect(document.querySelectorAll('.resto__title').item(0).textContent)
+        .toEqual('-');
+    });
 
-  it('should show the restos found by Favorite Restos', (done) => {
-    document.getElementById('resto-search-container')
-      .addEventListener('restos:searched:updated', () => {
-        expect(document.querySelectorAll('.resto').length).toEqual(3);
+    it('should show the restos found by Favorite Restos', (done) => {
+      document.getElementById('resto-search-container')
+        .addEventListener('restos:searched:updated', () => {
+          expect(document.querySelectorAll('.resto').length).toEqual(3);
+          done();
+        });
+
+      favoriteRestos.searchRestos.withArgs('resto a').and.returnValues([
+        { id: 111, title: 'resto abc' },
+        { id: 222, title: 'ada juga resto abcde' },
+        { id: 333, title: 'ini juga boleh resto a' },
+      ]);
+
+      searchRestos('resto a');
+    });
+
+    it('should show the name of the restos found by Favorite Restos', (done) => {
+      document.getElementById('resto-search-container').addEventListener('restos:searched:updated', () => {
+        const restoTitles = document.querySelectorAll('.resto__title');
+        expect(restoTitles.item(0).textContent).toEqual('resto abc');
+        expect(restoTitles.item(1).textContent).toEqual('ada juga resto abcde');
+        expect(restoTitles.item(2).textContent).toEqual('ini juga boleh resto a');
+
         done();
       });
 
-    FavoriteRestoIdb.searchRestos.withArgs('film a').and.returnValues([
-      { id: 111, title: 'film abc' },
-      { id: 222, title: 'ada juga film abcde' },
-      { id: 333, title: 'ini juga boleh film a' },
-    ]);
+      favoriteRestos.searchRestos.withArgs('resto a').and.returnValues([
+        { id: 111, title: 'resto abc' },
+        { id: 222, title: 'ada juga resto abcde' },
+        { id: 333, title: 'ini juga boleh resto a' },
+      ]);
 
-    searchRestos('film a');
+      searchRestos('resto a');
+    });
   });
 
-  it('should show the name of the restos found by Favorite Restos', (done) => {
-    document.getElementById('resto-search-container').addEventListener('restos:searched:updated', () => {
-      const restoTitles = document.querySelectorAll('.resto__title');
-      expect(restoTitles.item(0).textContent).toEqual('film abc');
-      expect(restoTitles.item(1).textContent).toEqual('ada juga film abcde');
-      expect(restoTitles.item(2).textContent).toEqual('ini juga boleh film a');
+  describe('When query is empty', () => {
+    it('should capture the query as empty', () => {
+      searchRestos(' ');
+      expect(presenter.latestQuery.length).toEqual(0);
 
-      done();
+      searchRestos('    ');
+      expect(presenter.latestQuery.length).toEqual(0);
+
+      searchRestos('');
+      expect(presenter.latestQuery.length).toEqual(0);
+
+      searchRestos('\t');
+      expect(presenter.latestQuery.length).toEqual(0);
     });
 
-    FavoriteRestoIdb.searchRestos.withArgs('film a').and.returnValues([
-      { id: 111, title: 'film abc' },
-      { id: 222, title: 'ada juga film abcde' },
-      { id: 333, title: 'ini juga boleh film a' },
-    ]);
+    it('should show all favorite restos', () => {
+      searchRestos('    ');
 
-    searchRestos('film a');
+      expect(favoriteRestos.getAllRestos)
+        .toHaveBeenCalled();
+    });
   });
-
 });
