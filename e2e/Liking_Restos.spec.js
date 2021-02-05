f328faed90034e95a44bfbb17f479cac876d4370
khairul anas <mafiakanan@gmail.com>
2021-02-05 08:44:48 +0700

    jangan sampai salah nama

--- e2e/Liking_Restos.spec.js
+++ e2e/Liking_Restos.spec.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 Feature('Liking Restos');
 
 Before(({ I }) => {
@@ -9,17 +11,23 @@ Scenario('showing empty liked restos', ({ I }) => {
   I.see('Tidak ada restoran untuk ditampilkan', '.resto-item__not__found');
 });
 
-Scenario('liking one resto', ({ I }) => {
+Scenario('liking one resto', async ({ I }) => {
   I.see('Tidak ada restoran untuk ditampilkan', '.resto-item__not__found');
 
   I.amOnPage('/');
   // pause();
   I.seeElement('.resto__title a');
-  I.click(locate('.resto__title a').first());
+  const firstresto = locate('.resto__title a').first();
+
+  const firstRestoTitle = await I.grabTextFrom(firstresto);
+  I.click(firstresto);
 
   I.seeElement('#likeButton');
   I.click('#likeButton');
 
   I.amOnPage('/#/favorite');
   I.seeElement('.post-item');
+
+  const likedRestoTitle = await I.grabTextFrom('.resto__title');
+  assert.strictEqual(firstRestoTitle, likedRestoTitle);
 });
